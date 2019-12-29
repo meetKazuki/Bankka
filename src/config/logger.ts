1487cef036a2eb074f89d6446e38997fe52659f5
Desmond Edem <edemintech@gmail.com>
2019-12-29 04:21:26 +0100

    chore(docs): setup documentation (#6)

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,34 +1,19 @@
 import appRoot from 'app-root-path';
-import { config } from 'dotenv';
-import { createLogger, format, transports } from 'winston';
+import winston from 'winston';
 
-config();
-
-const logger = createLogger({
-  format: format.combine(
-    format.align(),
-    format.simple(),
-    format.timestamp(),
-  ),
+const options: winston.LoggerOptions = {
   transports: [
-    new (transports.Console)({
-      format: format.combine(
-        format.align(),
-        format.colorize(),
-        format.simple(),
-        format.timestamp(),
-      ),
+    new winston.transports.Console({
       level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
-      handleExceptions: true,
     }),
-    new (transports.File)({
-      level: 'info',
-      filename: `${appRoot}/logs/app.info.log`,
-      handleExceptions: true,
+    new winston.transports.File({
+      level: 'error',
+      filename: `${appRoot}/logs/app.error.log`,
     }),
   ],
-  exitOnError: false,
-});
+};
+
+const logger = winston.createLogger(options);
 
 if (process.env.NODE_ENV !== 'production') {
   logger.debug('logging init at debug level');
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,19 @@
+import http from 'http';
 import app from './app';
 import logger from './config/logger';
 
-process.on('uncaughtException', e => {
-  logger.debug(e);
+process.on('uncaughtException', (e) => {
+  logger.error(e);
   process.exit(1);
 });
 
-process.on('unhandledRejection', e => {
-  logger.debug(e);
-  process.exit(1);
-});
+const PORT = process.env.PORT || 3000;
 
-app.set('port', process.env.PORT || 3000);
-app.set('env', process.env.NODE_ENV);
-
-const server = app.listen(app.get('port'), () => {
+const server = http.createServer(app);
+server.listen(PORT, () => {
   logger.debug(
-    `server running on http://localhost:${app.get('port')} in ${app.get('env')} mode`
+    `app running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode...press CTRL-C to stop🔥`,
   );
-  logger.debug('Press CTRL-C to stop');
 });
 
 export default server;
